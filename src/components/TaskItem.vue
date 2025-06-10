5cc2eab1fb217e06cede526d0be6bc309f96c73a
Teddy Nanta <teddy.nanta99@gmail.com>
2025-06-10 11:20:41 +0700

    add guarded input adn ux

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,89 +1,108 @@
 <script setup lang="ts">
-import type { Task } from '@/types/task'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { ref } from 'vue';
-import TaskList from './TaskList.vue';
+  import type { Task } from '@/types/task'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { fas } from '@fortawesome/free-solid-svg-icons';
+  import { nextTick, ref, watch } from 'vue';
 
-const emit = defineEmits<{
-  (event: 'toggle-done', id: number): void
-  (event: 'delete-task', id: number): void
-  (event: 'edit-task', id: number, title: string): void
-}>()
 
-const isEditing = ref(false)
-const editTitle = ref('')
+  const inputRef = ref<HTMLInputElement | null>(null)
 
-function editTask(title: string) {
-  isEditing.value = !isEditing.value
-  editTitle.value = title
-  console.log('isEditing', isEditing.value)
-} 
+  const isEditing = ref(false)
+  const editTitle = ref('')
 
-function submitEdit(id: number) {
-  if (!editTitle.value.trim()) return
-  emit('edit-task', {id: id, title: editTitle.value})
-  editTitle.value = ''
-  isEditing.value = false
-}
+  watch(isEditing, async (val) => {
+    if (val) {
+      await nextTick()
+      inputRef.value?.focus()
+    }
+  })
+  const emit = defineEmits<{
+    (event: 'toggle-done', id: number): void
+    (event: 'delete-task', id: number): void
+    (event: 'edit-task', id: number, title: string): void
+  }>()
 
-defineProps<{
-  task: Task
-}>()
+
+  function editTask(title: string) {
+    isEditing.value = !isEditing.value
+    editTitle.value = title
+    console.log('isEditing', isEditing.value)
+  }
+
+  function submitEdit(id: number) {
+    if (!editTitle.value.trim()) {
+      alert('Title cannot be empty')
+      return
+    }
+    emit('edit-task', { id: id, title: editTitle.value })
+    editTitle.value = ''
+    isEditing.value = false
+  }
+
+  defineProps<{
+    task: Task
+  }>()
 </script>
 
 <template>
   <div>
     <span v-if="isEditing">
       <form @submit.prevent="submitEdit(task.id)">
-        <input type="text" v-model="editTitle" placeholder="edit task" />
+        <input ref="inputRef" type="text" v-model="editTitle" placeholder="edit task" />
         <button class="green" type="submit">save</button>
         <button @click.prevent="editTask" class="red">cancel</button>
       </form>
     </span>
-    <span v-else :class="{done : task.isDone}">
+    <span v-else :class="{ done: task.isDone }">
       <input type="checkbox" id="checkbox" :checked="task.isDone" @change="emit('toggle-done', task.id)">
-      {{ task.title }}      
-      <FontAwesomeIcon @click="emit('delete-task', task.id)" :icon="fas.faTrash" class="red"/>
-      <FontAwesomeIcon @click="editTask(task.title)" :icon="fas.faPencil" class="yellow"/>
+      {{ task.title }}
+      <FontAwesomeIcon @click="emit('delete-task', task.id)" :icon="fas.faTrash" class="red" />
+      <FontAwesomeIcon @click="editTask(task.title)" :icon="fas.faPencil" class="yellow" />
     </span>
   </div>
 </template>
 
 <style>
-.done {
-  text-decoration: line-through;
-  color: gray;;
-}
+  .done {
+    text-decoration: line-through;
+    color: gray;
+    ;
+  }
 
-.red {
-  background-color: red;
-  color: white;
-  border: none;
-  padding: 5px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 10px;
-  /* margin-top: 5px; */
-}
+  .red {
+    background-color: red;
+    color: white;
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-left: 10px;
+    /* margin-top: 5px; */
+  }
 
-.yellow {
-  background-color: #f7e41d;
-  color: white;
-  border: none;
-  padding: 5px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 10px;
-}
+  input[type=text]:focus {
+    border: 1px solid cadetblue;
+    border-radius: 5px;
+    outline: none;
+  }
 
-.green {
-  background-color: green;
-  color: white;
-  border: none;
-  padding: 5px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 10px;
-}
+  .yellow {
+    background-color: #f7e41d;
+    color: white;
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-left: 10px;
+  }
+
+  .green {
+    background-color: green;
+    color: white;
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-left: 10px;
+  }
 </style>
